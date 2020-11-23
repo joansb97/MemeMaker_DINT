7cbade319bffc3cb25be4d0afdf57d50c2e00ed6
Joan Seva <joan.seva@iesdoctorbalmis.com>
2020-11-23 13:23:38 +0100

    Añado el converter

---
+++ MemeMaker_DINT/BordeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker_DINT
+{
+    class BordeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if ((bool)value == true) return 2;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is bool)
+            {
+                if ((bool)value == false) return 0;
+            }
+            return 3;
+        }
+    }
+}
