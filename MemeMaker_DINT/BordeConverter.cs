9735ca116024e02f96a6a47b6a0ffa7b5eed9c0d
Joan Seva <joan.seva@iesdoctorbalmis.com>
2020-11-23 13:41:09 +0100

    Modifico el conversor

--- MemeMaker_DINT/BordeConverter.cs
+++ MemeMaker_DINT/BordeConverter.cs
@@ -12,20 +12,14 @@ namespace MemeMaker_DINT
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if(value is bool)
-            {
-                if ((bool)value == true) return 2;
-            }
-            return 0;
+            if ((bool)value == true) return 2;
+            else return 0;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if(value is bool)
-            {
-                if ((bool)value == false) return 0;
-            }
-            return 3;
+            if ((bool)value == false) return 0;
+            else return 3;
         }
     }
 }
